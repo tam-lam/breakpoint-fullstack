487a137b941ef7fc02edf0f86258ed3b08d17491
Tam Lam <imtamtlam@gmail.com>
2019-02-18 16:27:07 +0700

    Initial Commit

---
+++ practice-full-stack/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  practice-full-stack
+//
+//  Created by Graphene on 18/2/19.
+//  Copyright © 2019 tam-lam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
